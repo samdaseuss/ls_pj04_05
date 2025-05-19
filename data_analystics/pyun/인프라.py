fa6f3f1b8918c89453cc03e478983601544b1958
tjdud2604 <redsloopjr@naver.com>
2025-05-19 09:32:59 +0900

    seoyoung

---
+++ data_analystics/pyun/인프라.py
@@ -0,0 +1,97 @@
+import pandas as pd
+import plotly.express as px
+import folium
+
+# 데이터 불러오기
+df3 = pd.read_csv("yc_busevent.csv")
+df3.head()
+
+df3['bus stop_name'].value_counts()
+
+df3 = df3.drop_duplicates(subset='bus stop_name')
+
+
+# 중심 위치 지정 (영천시 중심 좌표, 대략)
+map_center = [36.0, 128.9]
+
+# 지도 생성
+m = folium.Map(location=map_center, zoom_start=13)
+
+# 정류장마다 마커 추가
+for i, row in df3.iterrows():
+    folium.Marker(
+        location=[row['bus stop_gps y'], row['bus stop_gps x']],
+        popup=row['bus stop_name'],
+        icon=folium.Icon(color='green', icon='bus', prefix='fa')
+    ).add_to(m)
+
+m
+
+#####################################
+####################################3
+
+
+# 데이터 불러오기
+df4 = pd.read_csv("영천시_의료기관.csv", encoding='cp949')
+df4.head()
+
+
+# 중심 위치 지정 (영천시 중심 좌표, 대략)
+map_center = [35.9736, 128.9408]
+
+# 지도 생성
+m = folium.Map(location=map_center, zoom_start=13)
+
+for i, row in df4.iterrows():
+    folium.Marker(
+        location=[row['위도'], row['경도']],
+        popup=row['의료기관명'],
+        icon=folium.Icon(color='blue', icon='hospital', prefix='fa')
+    ).add_to(m)
+
+m
+
+#####################################
+####################################3
+
+df5 = pd.read_csv("경상북도 영천시_도시공원.csv", encoding='cp949')
+df5.head()
+
+# 중심 위치 지정 (영천시 중심 좌표, 대략)
+map_center = [35.9736, 128.9408]
+
+# 지도 생성
+m = folium.Map(location=map_center, zoom_start=13)
+
+for i, row in df5.iterrows():
+    folium.Marker(
+        location=[row['위도'], row['경도']],
+        popup=row['명칭'],
+        icon=folium.Icon(color='blue', icon='park', prefix='fa')
+    ).add_to(m)
+
+m
+
+
+
+from geopy.geocoders import Nominatim
+import pandas as pd
+import time
+
+# 지오코더 초기화
+geolocator = Nominatim(user_agent="yc-map")
+
+# 주소 → 위도·경도 함수
+def geocode(address):
+    try:
+        location = geolocator.geocode("영천시 " + address)
+        if location:
+            return pd.Series([location.latitude, location.longitude])
+    except:
+        return pd.Series([None, None])
+
+# 예제: 지번 주소만 있는 df
+df5[['위도', '경도']] = df5['지번'].apply(geocode)
+
+# 잠깐 쉬어가면서 (API 과부하 방지)
+time.sleep(1)
